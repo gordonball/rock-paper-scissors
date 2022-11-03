f84bee99a3e00a7ceb53c3361d398d8ebc1e33b5
Gordon Ball <gball261@gmail.com>
2022-11-02 23:32:36 -0400

    Changed button functionality

--- script.js
+++ script.js
@@ -5,50 +5,41 @@ const ROCK_PAPER_SCISSORS = ['ROCK', 'PAPER', 'SCISSORS'];
 const resultArea = document.getElementById("result-area")
 const playerChoiceArea = document.getElementById("player-selection");
 const computerChoiceArea = document.getElementById("computer-selection");
-const roundResult = document.getElementById("round-result")
+const roundResult = document.getElementById("round-result");
 
+let isWinner = false;
+
+let playerScore = 0;
+let computerScore = 0;
 
 function computerPlay() {
 	let hand = Math.floor(Math.random() * 3)
 	return ROCK_PAPER_SCISSORS[hand];
 }
 
-
-
-let playerScore = 0;
-let computerScore = 0;
-
 function playRound(playerSelection, computerSelection) {
 	let result = "";
-	console.log(playerSelection);
-	console.log(computerSelection);
+
 	if (playerSelection === computerSelection) {
 		let result = "Tie...";
-		console.log(result);
 	} else if (playerSelection === "ROCK" && computerSelection === "PAPER") {
 		computerScore++;
 		let result = "You lose...Paper covers Rock.";
-		console.log(result);
 	} else if (playerSelection === "ROCK" && computerSelection === "SCISSORS") {
 		playerScore++;
 		let result = "You win! Rock beats Scissors.";
-		console.log(result);
 	} else if (playerSelection === "PAPER" && computerSelection === "ROCK") {
 		playerScore++;
 		let result = "You win! Paper covers Rock.";
-		console.log(result);
 	} else if (playerSelection === "PAPER" && computerSelection === "SCISSORS") {
 		computerScore++;
 		let result = "You lose...Scissors beats Paper.";
-		console.log(result);
 	} else if (playerSelection === "SCISSORS" && computerSelection === "ROCK") {
 		computerScore++;
 		let result = "You lose...Rock beats Paper.";
-		console.log(result);
 	} else if (playerSelection === "SCISSORS" && computerSelection === "PAPER") {
 		playerScore++;
 		let result = "You win...Scissors beats Paper.";
-		console.log(result);
 	}
 
 	let round = `${result} The score is ${playerScore} to ${computerScore}`;
@@ -56,6 +47,8 @@ function playRound(playerSelection, computerSelection) {
 	let winner = checkWinner(playerScore, computerScore)
 	if (winner) {
 		roundResult.innerText = winner;
+		isWinner = true;
+		//TODO: append restart button
 		return winner
 	}
 
@@ -68,28 +61,25 @@ function playRound(playerSelection, computerSelection) {
 
 const rock = document.getElementById("rock");
 rock.addEventListener("click", (evt) => {
-	playRound(evt.target.value, computerPlay());
+	if (!isWinner) {
+		playRound(evt.target.value, computerPlay());
+	}
 });
 
 const paper = document.getElementById("paper");
 paper.addEventListener("click", (evt) => {
-	playRound(evt.target.value, computerPlay());
+	if (!isWinner) {
+		playRound(evt.target.value, computerPlay());
+	}
 });
 
 const scissors = document.getElementById("scissors");
 scissors.addEventListener("click", (evt) => {
-	playRound(evt.target.value, computerPlay());
+	if (!isWinner) {
+		playRound(evt.target.value, computerPlay());
+	}
 });
 
-
-
-
-// function game() {
-// 	let computerSelection = computerPlay();
-// 	let playerSelection =
-// 	playRound(playerSelection, computerSelection);
-
-// }
 
 
 function checkWinner(playerScore, computerScore) {
@@ -101,3 +91,5 @@ function checkWinner(playerScore, computerScore) {
 	return false
 }
 
+//TODO: add start button, populate game board?
+// game()
